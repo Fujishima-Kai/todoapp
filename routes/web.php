5772fcfc2b42e7fbd83d33d7fbc90f0ea3750de8
root <root@DESKTOP-9RG06OD.localdomain>
2020-04-05 18:52:13 +0900

    存在しないリレーションに対するエラーハンドリング設定

--- routes/web.php
+++ routes/web.php
@@ -19,21 +19,21 @@ Route::get('/', function () {
 
 
 Route::group(['middleware' => 'auth'], function() {
-    
     Route::get('/', 'HomeController@index')->name('home');
 
-    Route::get('/folders/{folder:id}/tasks', 'TaskController@index')->name('tasks.index');
-    Route::get('/posts/{post:slug}', 'PostController@show');
     Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
     Route::post('/folders/create', 'FolderController@create');
+    
+    Route::group(['middleware' => 'can:view,folder'], function() {
+        Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
 
     Route::get('/folders/{folder}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
     Route::post('/folders/{folder}/tasks/create', 'TaskController@create');
 
-
     Route::get('/folders/{folder}/tasks/{task}/edit', 'TaskController@showEditForm')->name('tasks.edit');
     Route::post('/folders/{folder}/tasks/{task}/edit', 'TaskController@edit');
-
+    });
 });
+
 
 Auth::routes();
